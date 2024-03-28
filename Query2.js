7b4967ead32a004089f79c3d8b44fa1178cce6e5
Zachary <zacharyh728@gmail.com>
2024-03-27 17:10:26 -0700

    fixed Query 2

--- Query2.js
+++ Query2.js
@@ -6,13 +6,22 @@ const client = new MongoClient(url);
 
 client.connect().then(() => {
     const tweets = client.db("CS3200_Assignment_5").collection("tweets");
-    tweets.find({}, { projection: { 'user.screen_name': 1, _id: 0 } }).sort({ 'user.followers_count': 1 }).limit(10).toArray().then(results => {
-        results.map(result => {
-            console.log(result.user.screen_name)
-        })
-        client.close();
-    })
-});
-
-
-
+    tweets.aggregate([
+        {
+            $group: {
+                _id: "$user.id",
+                followers_count: { $addToSet: "$user.followers_count" },
+                screen_name: { $addToSet: "$user.screen_name" }
+            }
+        }
+    ])
+        .sort({ followers_count: -1 })
+        .limit(10)
+        .toArray()
+        .then(results => {
+            results.map(result => {
+                console.log(result.screen_name[0], result.followers_count.sort((a, b) => { return b - a })[0]);
+            })
+            client.close();
+        });
+})
