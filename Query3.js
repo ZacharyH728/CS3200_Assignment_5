953f8da14d6a5420b9e4e152697d076ad9fba9ce
Zachary <zacharyh728@gmail.com>
2024-03-27 17:10:57 -0700

    finished query 3

---
+++ Query3.js
@@ -0,0 +1,30 @@
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
+const url = process.env.DATABASE_URL;
+const client = new MongoClient(url);
+
+client.connect().then(() => {
+    const tweets = client.db("CS3200_Assignment_5").collection("tweets");
+    tweets.aggregate([
+        {
+            $group: {
+                _id: "$user.id",
+                count: { $sum: 1 },
+                screen_name: { $addToSet: "$user.screen_name" }
+            }
+        }
+    ])
+        .sort({ count: -1 })
+        .limit(1)
+        .toArray()
+        .then(results => {
+            results.map(result => {
+                console.log(result.screen_name[0], result.count);
+            })
+            client.close();
+        })
+});
+
+
+
